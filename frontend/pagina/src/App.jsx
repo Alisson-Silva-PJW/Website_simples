b8394952d804b33b79a1b795ba6f5d06285821f2
Alisson Silva <al.s.projetosweb@gmail.com>
2023-08-20 23:20:10 -0300

    erro resolvido

--- frontend/pagina/src/App.jsx
+++ frontend/pagina/src/App.jsx
@@ -1,4 +1,14 @@
 //REACT
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+//COMPONENTS
+import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+
+//LAYOUT
+import Navbar from "./components/layout/Navbar";
 
 //CSS
 import "./App.css";
@@ -6,7 +16,16 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1>PROJETO PAGINA</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Products" element={<Products />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+        <p>Footer</p>
+      </BrowserRouter>
     </div>
   );
 }
